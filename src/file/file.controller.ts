cbee265ee66ae7c91c2c70ab34fbb92113a27304
Truong Pham <truongpham2412.nd@gmail.com>
2023-11-23 19:32:37 +0700

    bánh vẽ

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -3,17 +3,17 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
-  Get,
-  Param,
-  Res,
+  // Get,
+  // Param,
+  // Res,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { memoryStorage } from 'multer';
 import { editFileName, imageFileFilter } from './upload.utils';
-import { Response } from 'express';
+// import { Response } from 'express';
 import { FileService } from './file.service';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
-
+// import { readFileSync } from 'fs';
 @Controller('/api/v1/file')
 @ApiTags('files')
 export class FileController {
@@ -22,10 +22,12 @@ export class FileController {
   @Post()
   @UseInterceptors(
     FileInterceptor('image', {
-      storage: diskStorage({
-        destination: './files',
-        filename: editFileName,
-      }),
+      // storage: diskStorage({
+      //   destination: './files',
+      //   filename: editFileName,
+      // }),
+      // fileFilter: imageFileFilter,
+      storage: memoryStorage(),
       fileFilter: imageFileFilter,
     }),
   )
@@ -45,15 +47,19 @@ export class FileController {
   })
   //
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return {
-      message: `Upload thành công`,
-      success: true,
-      data: file.filename,
+    // const fileContent = readFileSync(file.path);
+
+    const params = {
+      Bucket: 'product',
+      Key: editFileName(file),
+      Body: file.buffer,
     };
+
+    return this.fileService.uploadToS3(params);
   }
 
-  @Get(':fileName')
-  async getFile(@Param('fileName') fileName: string, @Res() res: Response) {
-    return this.fileService.getFile(fileName, res);
-  }
+  // @Get(':fileName')
+  // async getFile(@Param('fileName') fileName: string, @Res() res: Response) {
+  //   return this.fileService.getFile(fileName, res);
+  // }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -2,8 +2,19 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { Response } from 'express';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 
-export class FileService {
+export class S3Service {
+  s3Uploader = new AWS.S3({
+    accessKeyId: 'Z1XTPE72PATXRGPARAFS',
+    secretAccessKey: 'FpMEAtv0sMneG5B6QyilUkfVtUIJfHmtwa5T3hkh',
+    endpoint: 's3.cloudfly.vn',
+    s3ForcePathStyle: true,
+  });
+}
+
+export class FileService extends S3Service {
   findFile(fileName: string): Promise<Buffer> {
     const filePath = path.join('./files', fileName);
 
@@ -14,6 +25,21 @@ export class FileService {
         } else {
           resolve(data);
         }
+      });
+    });
+  }
+
+  uploadToS3(params: S3.PutObjectRequest) {
+    return new Promise((resolve, reject) => {
+      this.s3Uploader.upload(params, (error: any, data: any) => {
+        if (error) {
+          reject();
+        }
+        resolve({
+          data: data?.Location,
+          message: 'Upload thành công',
+          success: true,
+        });
       });
     });
   }
